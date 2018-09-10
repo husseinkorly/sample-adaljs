45a26615cdd2d95e69677cf972f4304d2b944d73
Hussein Koprly <husseinkoprly@gmail.com>
2018-09-10 13:35:41 -0400

    minor fix

--- src/app/auth-n/auth-n.module.ts
+++ src/app/auth-n/auth-n.module.ts
@@ -4,10 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
 import { AuthGuardService } from './auth-guard.service';
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   imports: [CommonModule],
-  providers: [AuthService, AuthGuardService, AuthInterceptorService],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   declarations: []
 })
 export class AuthNModule {}
